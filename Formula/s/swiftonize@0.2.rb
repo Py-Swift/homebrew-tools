6f5ac4b7b6e0d61d166764f19eb30518fa69c12d
pythonswiftlink <pythonswiftlink@gmail.com>
2023-09-28 03:25:10 +0000

    Update from https://github.com/PythonSwiftLink/SwiftonizeExec/commit/d40aeebfc9978f03ea2b5b8d4390b3fb52d67ec7

--- Formula/s/swiftonize@0.2.rb
+++ Formula/s/swiftonize@0.2.rb
@@ -6,7 +6,7 @@ class SwiftonizeAT02 < Formula
 
   url "https://github.com/PythonSwiftLink/SwiftonizeExec/releases/download/0.2/swiftonize.tar.gz"
   version "0.2"
-  sha256 "477dfce6ae2605dc1aab2ccb4f981f3591d73304d61236b67bc7e42cb5e7da0f"
+  sha256 "bc0c0805a8685a83b08f8b6bb011bd67230c0aaa9666801f315a6df8aef244c2"
   license ""
 
   def install
