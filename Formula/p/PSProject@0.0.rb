abc152b580a695d4c53a76414038a77f8b91ef4b
pythonswiftlink <pythonswiftlink@gmail.com>
2023-10-09 07:41:59 +0000

    Update from https://github.com/PythonSwiftLink/PSProjectGenerator/commit/25adbef046468c07cb2d701141040ba72097bfc1

---
+++ Formula/p/PSProject@0.0.rb
@@ -0,0 +1,18 @@
+
+class PSProjectAT00 < Formula
+  desc ""
+  homepage ""
+
+  url "https://github.com/PythonSwiftLink/PSProjectGenerator/releases/download/0.0/PSProject.tar.gz"
+  version "0.0"
+  sha256 "9e5aa41558e35da42e7defd8afa8a4c1e65eee3746045edfd2bb06adb04eca00"
+  license ""
+
+  def install
+    bin.install "PSProject"
+    end
+
+  test do
+    system "false"
+  end
+end
