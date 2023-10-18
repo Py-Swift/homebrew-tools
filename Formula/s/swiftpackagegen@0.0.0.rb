1146ddc460020d49ca264a1ed19a94e04cfa1e38
pythonswiftlink <pythonswiftlink@gmail.com>
2023-10-18 13:29:16 +0000

    Update from https://github.com/PythonSwiftLink/SwiftPackageGen/commit/bbd2cb97ed895a2cd2c73d31f6bf0bbac33ea099

---
+++ Formula/s/swiftpackagegen@0.0.0.rb
@@ -0,0 +1,19 @@
+
+class SwiftpackagegenAT000 < Formula
+  depends_on "xcodegen"
+  desc ""
+  homepage ""
+
+  url "https://github.com/PythonSwiftLink/SwiftPackageGen/releases/download/0.0.0/SwiftPackageGen.tar.gz"
+  version "0.0.0"
+  sha256 "0340a0dce0a8057f871a81040b840dfe98258bbe6fe27bc5d7ced336f6f798b0"
+  license ""
+
+  def install
+    bin.install "SwiftPackageGen"
+    end
+
+  test do
+    system "false"
+  end
+end
