66d2889d4051c890d686c99428542d215448bdf2
psychowasp <psychowaspx@gmail.com>
2024-09-02 19:51:43 +0200

    Update psproject_master.rb

--- Formula/p/psproject_master.rb
+++ Formula/p/psproject_master.rb
@@ -4,9 +4,9 @@ class Psproject < Formula
   desc ""
   homepage ""
 
-  url "https://github.com/PythonSwiftLink/PSProjectGenerator/releases/download/0.0.7/PSProject.tar.gz"
-  version "0.0.7"
-  sha256 "4faaa36f99dd279440e9486d0cbc7aec7930d48fa905d54c000fb76da4e86ae7"
+  url "https://github.com/PythonSwiftLink/PSProjectGenerator/archive/master.zip"
+  version "master"
+  #sha256 "4faaa36f99dd279440e9486d0cbc7aec7930d48fa905d54c000fb76da4e86ae7"
   license ""
 
   def processor_count
