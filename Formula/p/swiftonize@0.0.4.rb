e329efe489954e0c6c7f36388eee4df04ef86b96
pythonswiftlink <pythonswiftlink@gmail.com>
2025-01-28 11:27:58 +0000

    Update from https://github.com/PythonSwiftLink/Swiftonize/commit/e3cd7636d195754033be4749725d6a2489382653

---
+++ Formula/p/swiftonize@0.0.4.rb
@@ -0,0 +1,19 @@
+
+class SwiftonizeAT004 < Formula
+  depends_on "xcodegen"
+  desc ""
+  homepage ""
+
+  url "https://github.com/PythonSwiftLink/SwiftonizeGenerator/releases/download/0.0.4/Swiftonize.tar.gz"
+  version "0.0.4"
+  sha256 "17233e3037627abfaf9ec1c7e2ba8073f2f4cba14be689e78b2d61a2561577e9"
+  license ""
+
+  def install
+    bin.install "Swiftonize"
+    end
+
+  test do
+    system "false"
+  end
+end
