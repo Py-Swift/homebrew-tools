9c6f923f8458e6bcec9b8422abfa02ac1ce2966b
pythonswiftlink <pythonswiftlink@gmail.com>
2025-08-12 16:15:33 +0000

    Update from https://github.com/Py-Swift/PSProjectGenerator/commit/82d7b3535812eba99d48fbdc5a38a5ff9bd8759a

--- Formula/p/psproject.rb
+++ Formula/p/psproject.rb
@@ -6,13 +6,13 @@ class Psproject < Formula
 
   url "https://github.com/PythonSwiftLink/PSProjectGenerator/releases/download/0.2.0/PSProject.tar.gz"
   version "0.2.0"
-  sha256 "5c0d0d80cab470a9bbe7f14f5e607fa9487e94547d433f87de59f145be9eee5f"
+  sha256 "3d559cf0542616a02aed3c18f4ccede32e35770ddc6a0bcf68836836a7221ca9"
   license ""
 
   def install
     bin.install "PSProject"
     bin.install "PythonSwiftProject_PSProjectGen.bundle"
-    bin.install "Python.framework"
+    lib.install "Python.framework"
     end
 
   test do
