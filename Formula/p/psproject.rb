6a17c612d2015aff9216bbcfdb401e5dd086262f
Py-Swift <107263226+Py-Swift@users.noreply.github.com>
2025-11-13 22:46:09 +0100

    Fix URL for PSProject release

--- Formula/p/psproject.rb
+++ Formula/p/psproject.rb
@@ -4,7 +4,7 @@ class Psproject < Formula
   desc ""
   homepage ""
 
-  url "https://github.com/PythonSwiftLink/PSProject/releases/download/1.0.0/PSProject.tar.gz"
+  url "https://github.com/Py-Swift/PSProject/releases/download/1.0.0/PSProject.tar.gz"
   version "1.0.0"
   sha256 "2ed6ddf404429c95f97a3155d3cb2f44eb6e18cfe8115aaf5fda4d2dd6aa8320"
   license ""
