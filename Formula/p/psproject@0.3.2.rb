29d2dab2ef9c9c4070a66c6e2e1cb8f1701ed160
psychowasp <psychowaspx@gmail.com>
2025-11-17 19:18:57 +0100

    Fix URL for PSProjectGenerator download

--- Formula/p/psproject@0.3.2.rb
+++ Formula/p/psproject@0.3.2.rb
@@ -4,7 +4,7 @@ class PsprojectAT032 < Formula
   desc ""
   homepage ""
 
-  url "https://github.com/PythonSwiftLink/PSProjectGenerator/releases/download/0.3.2/PSProject.tar.gz"
+  url "https://github.com/Py-Swift/PSProjectGenerator/releases/download/0.3.2/PSProject.tar.gz"
   version "0.3.2"
   sha256 "d21d11da0dff9659f1491b5eb65eb5c48a3e5f0c393ea450e693aa7d2ea92a2d"
   license ""
